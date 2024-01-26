c6dd7ca7e329c24dfcf47283e422faa05029705d
Ankit Singh <thakurbharat065@gmail.com>
2024-01-27 00:16:05 +0530

    Add_favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,48 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
- 
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-inter",
+});
+export const metadata: Metadata = {
+  title: "DevOverFlow",
+  description:
+    "A community-deriven platform for asking and answering programming question. Get help, Share knowledge, ans collaborate with develop from around the world Explore topic in wen development mobile app development,algorithms,data structure,ans more.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerAction: "primary-text-gradient hover:text-promary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">This is a place of text</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
